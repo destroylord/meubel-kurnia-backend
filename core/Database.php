92116440e12060dcfbd16f99355f94589868dd96
destroylord <masapin68@gmail.com>
2021-10-31 15:54:33 +0700

    feat(migrations): make a field table using migrations

--- core/Database.php
+++ core/Database.php
@@ -15,4 +15,67 @@ class Database
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $appliedMigrations  = $this->getAppliedMigrations();
+
+        $newMigrations = [];
+
+        $files = scandir(Application::$ROOT_DIR.'/migrations');
+
+        $toApplyMigrations = array_diff($files, $appliedMigrations);
+
+        foreach ($toApplyMigrations as $migration ) {
+            if ($migration === '.' || $migration === '..') {
+                continue;
+            }
+
+            require_once Application::$ROOT_DIR.'/migrations/'.$migration;
+            $className =  pathinfo($migration, PATHINFO_FILENAME);
+            $instance  = new $className();
+
+            echo "Applying migration $migration".PHP_EOL;
+            $instance->up();
+            echo "AppLIED migration $migration".PHP_EOL;
+        
+        }
+
+        if (!empty($newMigration)) {
+            $this->saveMigrations($newMigration);
+        }else{
+            $this->log("All migration are applied");
+        }
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;");
+    }
+
+    public function getAppliedMigrations()
+    {
+        $statments = $this->pdo->prepare("SELECT migration FROM migrations");
+        $statments->execute();
+
+        return $statments->fetchAll(\PDO::FETCH_COLUMN);
+    }
+    public function saveMigrations(array $migrations)
+    {
+        $str = implode(",", array_map(fn($m) => "('$m')", $migrations));
+        $statement = $this->pdo->prepare("INSERT INTO migrations (migration) VALUES 
+            $str
+        ");
+        $statement->execute();
+    }
+
+    protected function log($message)
+    {
+        echo "[". date('Y-m-d H:i:s') . "] - ".$message.PHP_EOL;
+    }
 }
---
+++ migrations.php
@@ -0,0 +1,21 @@
+<?php
+
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use app\core\Application;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn'       => $_ENV['DB_DSN'],
+        'user'      => $_ENV['DB_USER'],
+        'password'  => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application(__DIR__, $config);
+
+$app->db->applyMigrations();
