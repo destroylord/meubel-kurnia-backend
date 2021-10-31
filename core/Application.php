43eb919cd8eefd11177a4b3779e4f64b04398c24
destroylord <masapin68@gmail.com>
2021-10-31 11:24:35 +0700

    installation dotenv

--- core/Application.php
+++ core/Application.php
@@ -18,6 +18,8 @@ class Application {
         $this->request  = new Request();
         $this->response = new Response();
         $this->router   = new Router($this->request, $this->response);
+
+        $this->db = new Database();
     }
 
     public function getController(): \app\core\Controller //getter class
---
+++ core/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\core;
+
+class Database  
+{
+    public \PDO $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = new \PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+}
