d26b747b8a7876eb2ca5b9374e4b72c2fdae2362
destroylord <masapin68@gmail.com>
2021-11-21 22:07:14 +0700

    feat(dashboard): add new feature count in dashboard

---
+++ models/dashboard/report.php
@@ -0,0 +1,13 @@
+<?php
+
+    include_once $_SERVER['DOCUMENT_ROOT'].'/core/config.php';
+
+$count_users              = mysqli_query($conn, "SELECT COUNT(*) as count FROM users");
+$count_furniture          = mysqli_query($conn, "SELECT COUNT(*) as count FROM furnitures");
+$count_category_furniture = mysqli_query($conn, "SELECT COUNT(*) as count FROM categories_furniture");
+$count_order              = mysqli_query($conn, "SELECT COUNT(*) as count FROM orders");
+
+$ex_users        = mysqli_fetch_assoc($count_users);
+$ex_furnitures   = mysqli_fetch_assoc($count_furniture);
+$ex_categories   = mysqli_fetch_assoc($count_category_furniture);
+$ex_orders       = mysqli_fetch_assoc($count_order);
--- resources/dashboard/index.php
+++ resources/dashboard/index.php
@@ -1,26 +1,28 @@
 <?php include_once '../layouts/app_header.php'; ?>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT']."/models/dashboard/report.php"; ?>
         <!-- page content -->
         <div class="right_col" role="main">
           <div class="row" style="display: inline-block;">
               <div class=" top_tiles" style="margin: 10px 0;">
                 <div class="col-md-3 col-sm-3  tile">
-                  <span>Total Sessions</span>
-                  <h2>231,809</h2>
+                  <span>Total Order</span>
+                  <h2><?=$ex_orders['count'];?></h2>
                   <span class="sparkline_one" style="height: 160px;"><canvas width="478" height="125" style="display: inline-block; width: 478px; height: 125px; vertical-align: top;"></canvas></span>
                 </div>
                 <div class="col-md-3 col-sm-3  tile">
-                  <span>Total Revenue</span>
-                  <h2>$ 231,809</h2>
+                  <span>Total Furniture</span>
+                  <h2><?=$ex_furnitures['count']?></h2>
                   <span class="sparkline_one" style="height: 160px;"><canvas width="478" height="125" style="display: inline-block; width: 478px; height: 125px; vertical-align: top;"></canvas></span>
                 </div>
                 <div class="col-md-3 col-sm-3  tile">
-                  <span>Total Sessions</span>
-                  <h2>231,809</h2>
+                  <span>Total Category Furniture</span>
+                  <h2><?=$ex_categories['count']?></h2>
                   <span class="sparkline_one" style="height: 160px;"><canvas width="478" height="125" style="display: inline-block; width: 478px; height: 125px; vertical-align: top;"></canvas></span> 
                 </div>
                 <div class="col-md-3 col-sm-3  tile">
-                  <span>Total Sessions</span>
-                  <h2>231,809</h2>
+                  <span>Total Users</span>
+                  <h2><?=$ex_users['count']?></h2>
                   <span class="sparkline_one" style="height: 160px;"><canvas width="478" height="125" style="display: inline-block; width: 478px; height: 125px; vertical-align: top;"></canvas></span>
                 </div>
               </div>
