a2a52b639e1313199e9340a979f22101d84f7ac6
destroylord <masapin68@gmail.com>
2021-11-22 16:54:47 +0700

    feat(api): add feature show furniture by Id

---
+++ api/v1/furniture/showById.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/core/config.php';
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM furnitures WHERE id = '$id' ";
+$ex = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_object($ex)) {
+    $data[] = $row;
+}
+
+if ($data) {
+    $response = [
+        'status' => 200,
+        'msg'    => 'Success',
+        'data'   => $data
+    ];
+}else {
+    $response = [
+        'status' => 201,
+        'msg'    => 'Bad Request',
+    ];
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
