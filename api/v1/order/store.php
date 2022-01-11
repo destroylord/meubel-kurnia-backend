fa3efdf4feaad09e038e91f75e5473aa278c8efe
destroylord <masapin68@gmail.com>
2022-01-11 21:51:46 +0700

    fix: update api order store

--- api/v1/order/store.php
+++ api/v1/order/store.php
@@ -3,15 +3,14 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/core/config.php';
 
 $kode           = $_POST['kode'];
-$id_checkout    = $_POST['id_checkout'];
-$d_user         = $_POST['id_user'];
+$id_user        = $_POST['id_user'];
 $id_furniture   = $_POST['id_furniture'];
 $quantity       = $_POST['quantity'];
 $price          = $_POST['price'];
 $location       = $_POST['location'];
 $time           = date("Y-m-d H:i:s");
 
-$sql = "INSERT INTO orders(kode, id_checkout, price, location, created_at) VALUES('$kode', '$id_checkout', '$price', '$location', '$time')";
+$sql = "INSERT INTO orders(kode, id_user, id_furniture, quantity, price, location, created_at) VALUES('$kode', '$id_checkout', '$price', '$location', '$time')";
 
 $ex = mysqli_query($conn, $sql);
 
